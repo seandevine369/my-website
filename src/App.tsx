16b8236852f7bfc0b319a9a281a3e74a6b8baf79
Sean Devine <sdevine.ca@gmail.com>
2025-10-02 15:32:15 -0400

    Feat: integrated snap-scroll

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ export default function App() {
     return (
         <div className="flex flex-col min-h-screen">
             <Navbar />
-            <main className="flex-grow">
+            <main className="flex-grow snap-y h-screen overflow-y-scroll snap-proximity">
                 <HomeSection />
                 <AboutSection />
                 <ExperienceSection />
