067f1457bbd0e74ceae5413468652ae6f7bf0e0a
Sean Devine <sdevine.ca@gmail.com>
2025-10-20 16:30:56 -0400

    refactor: removed redundant sticky scroll (it is applied globally)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,7 +15,7 @@ export default function Navbar() {
                 <Logo
                     className="h-5 w-auto cursor-pointer"
                     onClick={() =>
-                        window.scrollTo({ top: 0, left: 0, behavior: "smooth" })
+                        window.scrollTo({ top: 0, left: 0 })
                     }
                 />
                 <div className="text-white space-x-6">
