eb3d4b83b9875630d56ee89b1414ac6b8476a5ea
Sean Devine <sdevine.ca@gmail.com>
2025-10-20 16:30:07 -0400

    chore: integrated locomotive scroll

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,31 @@ import ProjectsSection from "./sections/ProjectsSection";
 import ContactSection from "./sections/ContactSection";
 import ExperienceSection from "./sections/ExperienceSection";
 import SkillsSection from "./sections/SkillsSection";
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom";
+import locomotiveScroll from "locomotive-scroll";
+import 'locomotive-scroll/dist/locomotive-scroll.css'
+
 
 export default function App() {
+    const scrollRef = React.createRef();
+
+    useEffect(() => {
+        const scroll = new locomotiveScroll({
+            el: scrollRef.current,
+            smooth: true,
+            smoothMobile: true,
+            inertia: 0.01, // lower = more floaty, 1 = instant
+            getDirection: true,
+            getSpeed: true,
+        });
+    });
+
     return (
         <div>
+            <div data-scroll-container className="scroll" ref={scrollRef}>
             <Navbar />
-            <main className="flex-grow h-screen overflow-y-scroll snap-y snap-manditory">
+            <main className="flex-grow h-screen ">
                 <HomeSection />
                 <AboutSection />
                 <ExperienceSection />
@@ -18,6 +37,7 @@ export default function App() {
                 <SkillsSection />
                 <ContactSection />
             </main>
+            </div>
         </div>
     );
 }
